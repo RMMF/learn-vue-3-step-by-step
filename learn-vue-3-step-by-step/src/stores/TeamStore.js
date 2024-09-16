8da1d05ca2eb81ad10c39fdc239f8a7e9495d251
Rui <rui95fer@gmail.com>
2024-09-16 10:53:15 +0100

    new app

---
+++ learn-vue-3-step-by-step/src/stores/TeamStore.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+export let useTeamStore = defineStore('team', {
+
+    state() {
+        return {
+            name: '',
+            spots: 0,
+            members: []
+        }
+    },
+
+    actions: {
+        fill() {
+            import('@/team.json').then(r => {
+                console.log(r.default)
+            });
+        }
+    },
+});
