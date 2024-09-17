09aeec030ed37d68e83a5924e466cc2ba72b269c
Rui <rui95fer@gmail.com>
2024-09-17 21:54:22 +0100

    Build a Modal Component

--- learn-vue-3-step-by-step/src/views/TeamView.vue
+++ learn-vue-3-step-by-step/src/views/TeamView.vue
@@ -3,18 +3,39 @@ import TeamHeader from '@/components/Teams/TeamHeader.vue'
 import TeamMembers from '@/components/Teams/TeamMembers.vue'
 import TeamFooter from '@/components/Teams/TeamFooter.vue'
 import { useTeamStore } from '@/stores/TeamStore.js'
+import Modal from '@/components/Modal.vue'
+import { ref } from 'vue'
 
 let team = useTeamStore()
 
 team.fill()
+
+let showModal = ref(false)
 </script>
 
 <template>
-  <TeamHeader />
+  <TeamHeader @open="showModal = true" />
 
   <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
     <TeamMembers />
   </div>
 
   <TeamFooter />
+
+  <Modal :show="showModal" @close="showModal = false">
+    <template #header>Terms of Service</template>
+
+    <template #body-one>
+      With less than a month to go before the European Union enacts new consumer privacy laws for
+      its citizens, companies around the world are updating their terms of service agreements to
+      comply.
+    </template>
+
+    <template #body-two>
+      The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25
+      and is meant to ensure a common set of data rights in the European Union. It requires
+      organizations to notify users as soon as possible of high-risk data breaches that could
+      personally affect them.
+    </template>
+  </Modal>
 </template>
