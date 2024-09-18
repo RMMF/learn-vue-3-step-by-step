0a3021f9a359e7086c641b21b96cfe07e40302e0
rui-fernandes-dudev <rui.fernandes@dudev.pt>
2024-09-18 11:04:46 +0100

    Teleporting

--- learn-vue-3-step-by-step/src/views/TeamView.vue
+++ learn-vue-3-step-by-step/src/views/TeamView.vue
@@ -22,20 +22,22 @@ let showModal = ref(false)
 
   <TeamFooter />
 
-  <Modal :show="showModal" @close="showModal = false">
-    <template #header>Terms of Service</template>
+  <Teleport to="body">
+    <Modal :show="showModal" @close="showModal = false">
+      <template #header>Terms of Service</template>
 
-    <template #body-one>
-      With less than a month to go before the European Union enacts new consumer privacy laws for
-      its citizens, companies around the world are updating their terms of service agreements to
-      comply.
-    </template>
+      <template #body-one>
+        With less than a month to go before the European Union enacts new consumer privacy laws for
+        its citizens, companies around the world are updating their terms of service agreements to
+        comply.
+      </template>
 
-    <template #body-two>
-      The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25
-      and is meant to ensure a common set of data rights in the European Union. It requires
-      organizations to notify users as soon as possible of high-risk data breaches that could
-      personally affect them.
-    </template>
-  </Modal>
+      <template #body-two>
+        The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May
+        25 and is meant to ensure a common set of data rights in the European Union. It requires
+        organizations to notify users as soon as possible of high-risk data breaches that could
+        personally affect them.
+      </template>
+    </Modal>
+  </Teleport>
 </template>
